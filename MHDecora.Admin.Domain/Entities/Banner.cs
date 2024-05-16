24f16b630284d4fee0ac87f774e81e9645dc5081
Carlos Ribeiro <caduteruel@outlook.com>
2024-05-16 18:39:52 -0300

    Inicio Config EF Core + Oracle

--- MHDecora.Admin.Domain/Entities/Banner.cs
+++ MHDecora.Admin.Domain/Entities/Banner.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MHDecora.Admin.Domain.Entities
 {
+    [Table("MHBanners", Schema = "DECORAPHP")]
     public class Banner
     {
         public Guid Id { get; set; }
         public string Descricao { get; set; }
         public string CaminhoImagem { get; set; }
+        public int Ordem { get; set; }
     }
 }
--- MHDecora.Admin.Infra/AdminContext.cs
+++ MHDecora.Admin.Infra/AdminContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace MHDecora.Admin.Infra
 {
@@ -14,6 +15,15 @@ namespace MHDecora.Admin.Infra
         {
         }
 
-        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Banner> MHBanners { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Banner>()
+            //    .ToTable("MHBanners", "DECORAPHP") // Define o nome da tabela e o schema
+            //    .HasOracleSchema("DECORAPHP"); // Define o schema Oracle
+        }
     }
 }
