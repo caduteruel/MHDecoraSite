644577f69c53a742be148d1b38f35926299fd2ff
Rodrigo <rodrigo.dassi@outlook.com>
2024-05-23 19:41:48 -0300

    Ajustado Alerts.

--- MHDecora.Admin/Controllers/BannerController.cs
+++ MHDecora.Admin/Controllers/BannerController.cs
@@ -1,37 +1,21 @@
 ﻿using MHDecora.Admin.Application.Interfaces;
 using MHDecora.Admin.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace MHDecora.Admin.Controllers
 {
     public class BannerController : Controller
     {
-        private readonly List<Banner> _banners;
-        private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IBannerService _bannerService;
 
-        public BannerController(IWebHostEnvironment webHostEnvironment, IBannerService bannerService)
+        public BannerController(IBannerService bannerService)
         {
-            _banners = new List<Banner>();
-            _webHostEnvironment = webHostEnvironment;
             _bannerService = bannerService;
         }
 
         public async Task<IActionResult> Index()
         {
-
             var banners = await _bannerService.GetBanners();
-
-            //List<Banner> bannerList = new List<Banner>();
-
-            //Banner banner = new Banner();
-            //banner.Id = 1;
-            //banner.CaminhoImagem = "../images/banner/imagem1.jpeg";
-            //banner.Ordem = 1;
-            //banner.Descricao = "/Teste";
-
-            //bannerList.Add(banner);
 
             return View(banners);
         }
@@ -50,6 +34,9 @@ namespace MHDecora.Admin.Controllers
         public async Task<IActionResult> Adicionar(Banner banner, IFormFile arquivo)
         {
             await _bannerService.Criar(banner, arquivo);
+
+            Alert("Sucesso!", "O Banner foi cadastrado.", "success");
+
             return RedirectToAction("Index");
         }
 
@@ -63,20 +50,24 @@ namespace MHDecora.Admin.Controllers
         public async Task<IActionResult> Excluir(int bannerId)
         {
             bool result = await _bannerService.Excluir(bannerId);
+
             if (result)
             {
-                return Ok("Banner excluído com Sucesso");
+                Alert("Sucesso!", "O Banner foi excluído.", "success");
             }
             else
             {
-                return BadRequest("Não foi possível excluir o Banner. Verifique.");
-            }           
+                Alert("Erro!", "Não foi possível excluir o banner, contate a equipe de suporte.", "danger");
+            }
+
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetBannerById(int bannerId)
         {
             await _bannerService.GetBannerById(bannerId);
+
             return Ok();
         }
 
@@ -84,11 +75,6 @@ namespace MHDecora.Admin.Controllers
         public async Task<IActionResult> Editar(int bannerId)
         {
             var banner = await _bannerService.GetBannerById(bannerId);
-            //Banner banner = new Banner();
-            //banner.Id = 1;
-            //banner.CaminhoImagem = "../images/banner/imagem1.jpeg";
-            //banner.Ordem = 1;
-            //banner.Descricao = "/Teste";
 
             return View(banner);
         }
@@ -96,20 +82,18 @@ namespace MHDecora.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Editar(IFormFile arquivo, Banner banner)
         {
-
             var result = await _bannerService.Editar(arquivo, banner);
 
-            if (!result)
+            if (result)
             {
-                return BadRequest("Banner não encontrado.");
+                Alert("Sucesso!", "O Banner foi editado.", "success");
+            }
+            else
+            {
+                Alert("Erro!", "Não foi possível editar o banner, contate a equipe de suporte.", "danger");
             }
 
-            return Ok("Index");
-        }
-
-        public class ArquivoViewModel
-        {
-            public IFormFile Arquivo { get; set; }
+            return RedirectToAction("Index");
         }
 
         private void Alert(string titulo, string mensagem, string alerta)
