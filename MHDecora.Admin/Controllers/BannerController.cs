6ff0b859a49f05f26bdb279173de5bd136fafb05
Rodrigo <rodrigo.dassi@outlook.com>
2024-05-22 22:00:23 -0300

    Implementado adicionar.

--- MHDecora.Admin/Controllers/BannerController.cs
+++ MHDecora.Admin/Controllers/BannerController.cs
@@ -1,6 +1,7 @@
 ﻿using MHDecora.Admin.Application.Interfaces;
 using MHDecora.Admin.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MHDecora.Admin.Controllers
 {
@@ -19,9 +20,20 @@ namespace MHDecora.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
-            
-            var banners = await _bannerService.GetBanners();
-            return View(banners);
+
+            //var banners = await _bannerService.GetBanners();
+
+            List<Banner> bannerList = new List<Banner>();
+
+            Banner banner = new Banner();
+            banner.Id = 1;
+            banner.CaminhoImagem = "../images/banner/imagem1.jpeg";
+            banner.Ordem = 1;
+            banner.Descricao = "/Teste";
+
+            bannerList.Add(banner);
+
+            return View(bannerList);
         }
 
         [HttpPost]
@@ -32,9 +44,9 @@ namespace MHDecora.Admin.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Adicionar(Banner banner, IFormFile imagem)
+        public async Task<IActionResult> Adicionar(Banner banner, IFormFile arquivo)
         {
-            await _bannerService.Criar(banner, imagem);
+            await _bannerService.Criar(banner, arquivo);
             return RedirectToAction("Index");
         }
 
@@ -68,9 +80,39 @@ namespace MHDecora.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> Editar(int bannerId)
         {
-            var responde = await _bannerService.Editar(bannerId);
+            //var responde = await _bannerService.Editar(bannerId);
+            Banner banner = new Banner();
+            banner.Id = 1;
+            banner.CaminhoImagem = "../images/banner/imagem1.jpeg";
+            banner.Ordem = 1;
+            banner.Descricao = "/Teste";
 
-            return View(responde);
+            return View(banner);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(IFormFile arquivo, Banner banner)
+        {
+            if (arquivo == null || arquivo.Length == 0)
+            {
+                // Lógica para lidar com o envio sem arquivo selecionado
+                return BadRequest("Nenhum arquivo selecionado.");
+            }
+
+            // Lógica para salvar o arquivo
+            var filePath = Path.Combine("wwwroot/banner", arquivo.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+
+            // Lógica para redirecionar ou retornar uma resposta
+            return RedirectToAction("Index", "Home");
+        }
+
+        public class ArquivoViewModel
+        {
+            public IFormFile Arquivo { get; set; }
         }
     }
 }
