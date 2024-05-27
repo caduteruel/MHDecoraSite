0d31981764bb0ec3cebc8cc34f521b0441efc73d
Carlos Ribeiro <caduteruel@outlook.com>
2024-05-27 14:57:33 -0300

    Add Controller

---
+++ MHDecora.Admin/Controllers/QuemSomosController.cs
@@ -0,0 +1,38 @@
+﻿using MHDecora.Admin.Application.Interfaces;
+using MHDecora.Admin.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHDecora.Admin.Controllers
+{
+    public class QuemSomosController : Controller
+    {
+        private readonly IQuemSomosService _quemSomosService;
+
+        public QuemSomosController(IQuemSomosService quemSomosService)
+        {
+            _quemSomosService = quemSomosService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(QuemSomos dados, IFormFile imagem)
+        {
+            await _quemSomosService.Salvar(dados, imagem);
+
+            Alert("Sucesso!", "Seção Quem Somos foi atualizada.", "success");
+
+            return RedirectToAction("Index");
+        }
+
+        private void Alert(string titulo, string mensagem, string alerta)
+        {
+            TempData["Titulo"] = titulo;
+            TempData["Mensagem"] = mensagem;
+            TempData["TipoAlerta"] = alerta;
+        }
+    }
+}
