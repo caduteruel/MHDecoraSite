01daebc48a8dd83f6313b09bc668f127a9f6fda3
Carlos Ribeiro <caduteruel@outlook.com>
2024-05-28 18:06:07 -0300

    Add Categoria e Tag

---
+++ MHDecora.Admin/Controllers/CategoriaController.cs
@@ -0,0 +1,67 @@
+﻿using MHDecora.Admin.Application.Interfaces;
+using MHDecora.Admin.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHDecora.Admin.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriaController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categorias = await _categoriaService.GetCategorias();
+
+            return View(categorias);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Categoria categoria)
+        {
+            await _categoriaService.Criar(categoria);
+
+            Alert("Sucesso!", "A Categoria foi cadastrada.", "success");
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var banner = await _categoriaService.GetCategoriaById(id);
+
+            return View(banner);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Excluir(int id)
+        {
+            bool result = await _categoriaService.Excluir(id);
+
+            if (result)
+            {
+                Alert("Sucesso!", "A categoria foi excluída.", "success");
+            }
+            else
+            {
+                Alert("Erro!", "Não foi possível excluir a categoria, contate a equipe de suporte.", "danger");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        private void Alert(string titulo, string mensagem, string alerta)
+        {
+            TempData["Titulo"] = titulo;
+            TempData["Mensagem"] = mensagem;
+            TempData["TipoAlerta"] = alerta;
+        }
+    }
+}
---
+++ MHDecora.Admin/Controllers/TagController.cs
@@ -0,0 +1,67 @@
+﻿using MHDecora.Admin.Application.Interfaces;
+using MHDecora.Admin.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHDecora.Admin.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagService _tagService;
+
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tags = await _tagService.GetTags();
+
+            return View(tags);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Tag tag)
+        {
+            await _tagService.Criar(tag);
+
+            Alert("Sucesso!", "A Tag foi cadastrada.", "success");
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var tag = await _tagService.GetTagById(id);
+
+            return View(tag);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Excluir(int id)
+        {
+            bool result = await _tagService.Excluir(id);
+
+            if (result)
+            {
+                Alert("Sucesso!", "A tag foi excluída.", "success");
+            }
+            else
+            {
+                Alert("Erro!", "Não foi possível excluir a tag, contate a equipe de suporte.", "danger");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        private void Alert(string titulo, string mensagem, string alerta)
+        {
+            TempData["Titulo"] = titulo;
+            TempData["Mensagem"] = mensagem;
+            TempData["TipoAlerta"] = alerta;
+        }
+    }
+}
