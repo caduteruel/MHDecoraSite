5fbbc7ce0519a9452dd6122d93665de058a39fb8
Rodrigo <rodrigo.dassi@outlook.com>
2024-05-28 09:36:12 -0300

    Implementado Montagem

---
+++ MHDecora.Admin/Controllers/ConfiguracaoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MHDecora.Admin.Controllers
+{
+    public class ConfiguracaoController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Configurar()
+        {
+            return View();
+        }
+    }
+}
