2100184b596ec94004e591a04f903ea508a7eb78
Rodrigo <rodrigo.dassi@outlook.com>
2024-05-28 12:35:55 -0300

    Implementado Tema.

---
+++ MHDecora.Admin/Controllers/TemaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MHDecora.Admin.Controllers
+{
+    public class TemaController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var response = await _montagemService.Buscar();
+
+            if (response == null)
+            {
+                Alert("Erro!", "Não foi possível carregar os dados, contate a equipe de suporte.", "danger");
+
+                return View(response);
+            }
+
+            return View(response);
+        }
+    }
+}
