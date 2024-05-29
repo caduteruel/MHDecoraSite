e08591f88309dc202b416e82eb34d80ec668c237
Rodrigo <rodrigo.dassi@outlook.com>
2024-05-29 15:19:33 -0300

    Contato

---
+++ MHDecora.Admin.Domain/Entities/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHDecora.Admin.Domain.Entities
+{    
+    public class Contato
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string CaminhoImagem { get; set; }
+        public int Ordem { get; set; }
+    }
+}
