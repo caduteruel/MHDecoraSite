ae9f315fa5da5f090ebed4fe5b5221db4ecc9a00
Rodrigo <rodrigo.dassi@outlook.com>
2024-06-05 23:40:05 -0300

    Novos arquivos

---
+++ MHDecora.Admin/Controllers/OrcamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MHDecora.Admin.Controllers
+{
+    public class OrcamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
