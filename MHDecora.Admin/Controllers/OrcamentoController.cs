02a64e968258b4299f26f492cc239628cb6d9a71
Rodrigo Dassi Pascoal <rodrigo.dassi@outlook.com>
2024-06-08 16:00:10 -0300

    Contato e icones.

--- MHDecora.Admin/Controllers/OrcamentoController.cs
+++ MHDecora.Admin/Controllers/OrcamentoController.cs
@@ -1,21 +1,40 @@
 ﻿using MHDecora.Admin.Application.Interfaces;
+using MHDecora.Admin.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MHDecora.Admin.Controllers
 {
     public class OrcamentoController : Controller
     {
-        //private readonly IOrcamentoService _orcamentoService;
+        private readonly IOrcamentoService _orcamentoService;
 
-        //public OrcamentoController(IOrcamentoService orcamentoService)
-        //{
-        //    _orcamentoService = orcamentoService;
-        //}
-
-
-        public IActionResult Index()
+        public OrcamentoController(IOrcamentoService orcamentoService)
         {
-            return View();
+            _orcamentoService = orcamentoService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Orcamento orcamento)
+        {
+            var response = await _orcamentoService.Salvar(orcamento);
+
+            if (response)
+            {
+                Alert("Sucesso!", "Orçamento enviado, aguarde nosso contato.", "success");
+            }
+            else
+            {
+                Alert("Erro!", "Não foi possível enviar o orçamento, contate por telefone.", "danger");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private void Alert(string titulo, string mensagem, string alerta)
+        {
+            TempData["Titulo"] = titulo;
+            TempData["Mensagem"] = mensagem;
+            TempData["TipoAlerta"] = alerta;
         }
     }
 }
