e9b8e49c68a21d9dea3c35bd06e92a87a29b563b
Carlos Ribeiro <caduteruel@outlook.com>
2024-06-09 15:50:53 -0300

    Remove LinkTema

--- MHDecora.Admin.Domain/Entities/Tema.cs
+++ MHDecora.Admin.Domain/Entities/Tema.cs
@@ -10,7 +10,6 @@ namespace MHDecora.Admin.Domain.Entities
         public string CaminhoImagem { get; set; }
         public string Texto { get; set; }
         public string Titulo { get; set; }
-        public string LinkTema { get; set; }
         public string? Tags { get; set; }
         [NotMapped]
         public List<Tag> TagsList { get; set; }
