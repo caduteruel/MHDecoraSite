8ddeb7f28c20e3fa2c6d246428135b214f72d924
Rodrigo Dassi Pascoal <rodrigo.dassi@outlook.com>
2024-07-01 20:09:10 -0300

    Limpeza de código

--- MHDecora.Site.Infra/Repositories/MontagemRepository.cs
+++ MHDecora.Site.Infra/Repositories/MontagemRepository.cs
@@ -111,108 +111,33 @@ namespace MHDecora.Site.Infra.Repositories
                 
                 if (tema != null)
                 if (tema.Tags != null)
-                {
-                        //string[] listaIdTags = tema.Tags.Split(",");
+                {                       
+                    var listaTemaTags = tema.Tags.Split(",").Select(tag => tag.Trim()).ToList();
 
-                        //// Converter os IDs para inteiros (se necessário)
-                        //int[] ids = Array.ConvertAll(listaIdTags, int.Parse);
+                    var palavrasChaveTags = new HashSet<string>(listaTemaTags);
 
-                        //// Buscar os nomes das TAGs
-                        //var listaNomeTags = await _context.MH_TAGS
-                        //    .Where(x => ids.Contains(x.Id)) // Filtra apenas as tags cujos IDs estão na lista de IDs convertidos
-                        //    .Select(p => p.Nome) // Seleciona apenas o campo Nome
-                        //    .ToListAsync(); // Converte o resultado para uma lista assíncrona
+                    // Obter todas as montagens do banco de dados
+                    var todasMontagens = await _context.MH_MONTAGEM.Include(x => x.Categoria).AsNoTracking().ToListAsync();
 
-                        //// Converter listaNomeTags para um HashSet para otimização de busca
+                    // Filtrar em memória utilizando Any e Contains
+                    var listaMontagens = todasMontagens
+                        .Where(x => x.Tags != null && x.Tags.Split(",").Select(tag => tag.Trim()).Any(tag => palavrasChaveTags.Contains(tag)))
+                        .ToList();
 
-                        //if (listaNomeTags != null)
-                        //{
-                        //    var palavrasChave = new HashSet<string>(listaNomeTags);
-                        //    var palavrasChaveTags = new HashSet<string>(listaIdTags);
+                    foreach (var item1 in listaMontagens)
+                    {
+                        item1.CaminhoImagem = GetPathImagens() + item1.CaminhoImagem;
+                        item1.CaminhoImagem2 = GetPathImagens() + item1.CaminhoImagem2;
+                        item1.CaminhoImagem3 = GetPathImagens() + item1.CaminhoImagem3;
+                        item1.CaminhoImagem4 = GetPathImagens() + item1.CaminhoImagem4;
+                    }
 
-                        //    // Realiza a consulta inicial para obter todas as montagens
-                        //    var todasMontagens = await _context.MH_MONTAGEM.ToListAsync();
-
-                        //    // Filtra em memória utilizando expressões regulares para buscar parte do texto
-                        //    var regexPadrao = new Regex(string.Join("|", palavrasChave.Select(p => Regex.Escape(p))));
-
-
-                        //    var listaMontagens = todasMontagens
-                        //                                .Where(x => regexPadrao.IsMatch(x.Texto) ||
-                        //                                            regexPadrao.IsMatch(x.Titulo) ||
-                        //                                            regexPadrao.IsMatch(x.TextoImagem) ||
-                        //                                (x.Tags != null && palavrasChaveTags.Any(tag => x.Tags.Contains(tag))))
-                        //                            .ToList();
-
-                        //var listaTemaTags = tema.Tags.Split(",").ToList();
-
-                        //var palavrasChaveTags = new HashSet<string>(listaTemaTags);
-
-
-                        //// Realiza a consulta inicial para obter todas as montagens
-                           //var todasMontagens = await _context.MH_MONTAGEM.AsNoTracking().ToListAsync();
-
-                        //// Filtra em memória utilizando expressões regulares para buscar parte do texto
-                        //   var regexPadrao = new Regex(string.Join("|", palavrasChaveTags.Select(p => Regex.Escape(p))));
-
-
-                        //var listaMontagens = todasMontagens
-                        //                                .Where(x => x.Tags != null && palavrasChaveTags.Any(tag => x.Tags.Contains(listaTemaTags.Any(t => tag.Contains(t))))
-
-                        var listaTemaTags = tema.Tags.Split(",").Select(tag => tag.Trim()).ToList();
-
-                        var palavrasChaveTags = new HashSet<string>(listaTemaTags);
-
-                        // Obter todas as montagens do banco de dados
-                        var todasMontagens = await _context.MH_MONTAGEM.Include(x => x.Categoria).AsNoTracking().ToListAsync();
-
-                        // Filtrar em memória utilizando Any e Contains
-                        var listaMontagens = todasMontagens
-                            .Where(x => x.Tags != null && x.Tags.Split(",").Select(tag => tag.Trim()).Any(tag => palavrasChaveTags.Contains(tag)))
-                            .ToList();
-
-
-
-
-                        // 1. Carregar todas as montagens do banco de dados
-                        //var todasMontagens = _context.MH_MONTAGEM.AsNoTracking().ToList();
-
-
-                        foreach (var item1 in listaMontagens)
-                        {
-                            item1.CaminhoImagem = GetPathImagens() + item1.CaminhoImagem;
-                            item1.CaminhoImagem2 = GetPathImagens() + item1.CaminhoImagem2;
-                            item1.CaminhoImagem3 = GetPathImagens() + item1.CaminhoImagem3;
-                            item1.CaminhoImagem4 = GetPathImagens() + item1.CaminhoImagem4;
-                        }
-                          
-
-                            //foreach (var item in listaMontagens)
-                            //{
-                            //    item.CaminhoImagem = GetPathImagens() + item.CaminhoImagem;
-                            //    item.CaminhoImagem2 = GetPathImagens() + item.CaminhoImagem2;
-                            //    item.CaminhoImagem3 = GetPathImagens() + item.CaminhoImagem3;
-                            //    item.CaminhoImagem4 = GetPathImagens() + item.CaminhoImagem4;
-                            //}
-
-                        if (listaMontagens == null)
-                        {
-                            return new List<Montagem>();
-                        }
+                    if (listaMontagens == null)
+                    {
+                        return new List<Montagem>();
+                    }
                     
-                            return listaMontagens;
-
-                        
-                        // 2. Extrair todas as tags de todas as montagens
-                       // var todasTags = todasMontagens.SelectMany(montagem => montagem.Tags.Contains(listaTemaTags.First)).ToList();
-
-                        // 3. Filtrar as montagens com base nas tags desejadas
-                        //var listaMontagens = todasMontagens.Where(montagem => montagem.Tags.Any(tag => todasTags.Contains(tag))).ToList();
-
-
-
-
-
+                    return listaMontagens;
                 }
 
                 return new List<Montagem>();
@@ -236,7 +161,6 @@ namespace MHDecora.Site.Infra.Repositories
                     item.CaminhoImagem2 = GetPathImagens() + item.CaminhoImagem2;
                     item.CaminhoImagem3 = GetPathImagens() + item.CaminhoImagem3;
                     item.CaminhoImagem4 = GetPathImagens() + item.CaminhoImagem4;
-
                 }
 
                 return listaMontagens;
