02f68af8c0a7dcc35ceae04f0fb9af5339c3b2b7
Rodrigo Dassi Pascoal <rodrigo.dassi@outlook.com>
2024-07-03 00:05:07 -0300

    Ajustes

--- MHDecora.Admin.Application/obj/Release/net8.0/MHDecora.Admin.Application.AssemblyInfo.cs
+++ MHDecora.Admin.Application/obj/Release/net8.0/MHDecora.Admin.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MHDecora.Admin.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcb00d440912b8a9caf5d9827641e805239f20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0ec39ee10deb75920017a824c0ba88318e49e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MHDecora.Admin.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MHDecora.Admin.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MHDecora.Site.Domain/obj/Release/net8.0/MHDecora.Site.Domain.AssemblyInfo.cs
+++ MHDecora.Site.Domain/obj/Release/net8.0/MHDecora.Site.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MHDecora.Site.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcb00d440912b8a9caf5d9827641e805239f20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0ec39ee10deb75920017a824c0ba88318e49e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MHDecora.Site.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MHDecora.Site.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
