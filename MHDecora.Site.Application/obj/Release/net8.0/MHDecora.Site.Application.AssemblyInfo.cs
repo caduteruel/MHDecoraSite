78358c57cad69df3600e09104f252f324fff9e36
Rodrigo Dassi Pascoal <rodrigo.dassi@outlook.com>
2024-07-02 23:48:55 -0300

    Ajustes montaegns

--- MHDecora.Site.Application/obj/Release/net8.0/MHDecora.Site.Application.AssemblyInfo.cs
+++ MHDecora.Site.Application/obj/Release/net8.0/MHDecora.Site.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MHDecora.Site.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddeb7f28c20e3fa2c6d246428135b214f72d924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcb00d440912b8a9caf5d9827641e805239f20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MHDecora.Site.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MHDecora.Site.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
