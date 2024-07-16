cd45538decbb772d417b73140fe3176dad00d497
Rodrigo Dassi Pascoal <rodrigo.dassi@outlook.com>
2024-07-16 15:05:58 -0300

    Nome admin

--- MHDecora.Admin.Domain/obj/Release/net8.0/MHDecora.Admin.Domain.AssemblyInfo.cs
+++ MHDecora.Admin.Domain/obj/Release/net8.0/MHDecora.Admin.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MHDecora.Admin.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004e1805bbd43a5403b393974dde365c6064d8e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba8c376f681fd089ecbefa1c97ed21dafe5499f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MHDecora.Admin.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MHDecora.Admin.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
